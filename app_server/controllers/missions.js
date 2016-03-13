b36a37ae7595abf7690b173f05ac602b983c0692
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-12 21:00:08 -0500

    Add data from views to controllers

--- app_server/controllers/missions.js
+++ app_server/controllers/missions.js
@@ -1,5 +1,29 @@
 module.exports.missionlist = function(req, res){
-	res.render('missions-list', {title: 'Home'});
+	res.render('missions-list', {
+	 	title: 'AirLoft',
+	 	subtitle: 'Explore available missions around you!',
+	 	sidetext: 'Meet interesting people here and do great things! AirLoft helps you share your interests and habbits with the world',
+	 	missions: [
+	 		{
+	 			name: Nodejs Ninja,
+	 			rating: 3,
+	 			author: Shunzhe Yu,
+	 			tags: ['Nodejs', 'Javascript', 'Mongodb']
+	 		},
+	 		{
+	 			name: Reactjs Ninja,
+	 			rating: 4,
+	 			author: Shunzhe Yu,
+	 			tags: ['Nodejs', 'Javascript', 'Mongodb']
+	 		},
+	 		{
+	 			name: 'Database Ninja',
+	 			rating: 5,
+	 			author: Shunzhe Yu,
+	 			tags: ['NoSQL', 'postgresql', 'Mongodb']
+	 		}
+	 	]
+	});
 };
 
 module.exports.missioninfo = function(req, res){
