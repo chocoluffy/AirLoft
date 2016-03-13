48e830388b5ebc9e29cf539c98d289d781e4167d
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-13 17:58:29 -0400

    Moved all hard-cored data into controllers

--- app_server/controllers/missions.js
+++ app_server/controllers/missions.js
@@ -27,9 +27,56 @@ module.exports.missionlist = function(req, res){
 };
 
 module.exports.missioninfo = function(req, res){
-	res.render('mission-info', {title: 'Missions details'});
+	res.render('mission-info', {
+		name: 'Nodejs Ninja',
+		rating: 4,
+		author: 'Shunzhe Yu',
+		timePanel:{
+			title: 'Training Time',
+			timeslots: ['March 15 - March 17: 7:00 am - 7:00 pm', 
+			'March 21 - March 23: 7:00 am - 7:00 pm'
+			]
+		},
+		contentPanel: {
+			title: 'materials',
+			tags: [{
+				tagname: 'Nodejs',
+				tagdescript: '12 practical hand-on Nodejs projects'
+			},
+			{
+				tagname: 'Javascript',
+				tagdescript: 'Learning Javascript in 7 days'
+			},
+			{
+				tagname: 'Mongodb',
+				tagdescript: 'Using mongodb to store data'
+			}]
+		},
+		locationPanel: {
+			title: 'Location Map',
+			cords: ['43.666257', '-79.391198']
+		},
+		reviewPanel: {
+			title: 'Peer Reviews',
+			addReviewBtn: 'Add review',
+			reviews: [{
+				rating: 4,
+				author: 'Shunzhe Yu',
+				timestamp: '12 March 2016',
+				text: 'What a great training course! It is the best node tutorial I ever had. I hope that this website can be known by more people, and meet more like-minded people! Again, thanks!!'
+			},
+			{
+				rating: 5,
+				author: 'Monkey D Luffy',
+				timestamp: '14 March 2016',
+				text: 'What a great training course! One thing I did notice is that tutors are really patient and are willing to teach us a lot of useful techiniques that can be easily applied to our own projects! Thanks!'
+			}]
+		}
+	});
 };
 
 module.exports.addReview = function(req, res){
-	res.render('mission-review-form', {title: 'Add review'});
+	res.render('mission-review-form', {
+		name: 'Review Nodejs Ninja'
+	});
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,6 @@
 module.exports.about = function(req, res) {
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', {
+  	title: 'About us',
+  	text: 'AirLoft is created to help people share their habbits and insights with the world around them. Loft, refers to a private space you can have a deep involvement with things you like. That thing may be just a rolling music disk, a dazzling computer game, or even a playboy magazine.\n\n Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam quod, necessitatibus officiis totam inventore porro cum commodi velit provident excepturi soluta laboriosam quam ex, minus. Quis facilis pariatur, adipisci minus.'
+  });
 };
