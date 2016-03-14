a816b87aaf13e0908a73232a2e4bc23c4b9486ca
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-14 10:27:48 -0400

    Finished schema design

--- app_server/models/missions.js
+++ app_server/models/missions.js
@@ -1,8 +1,23 @@
 var mongoose = require('mongoose');
+
+var timeSchema = new mongoose.Schema({
+	title: String,
+	timeSlots: [String]
+});
+
+var reviewSchema = new mongoose.Schema({
+	rating: {type: Number, "default": 0, min: 0, max: 5},
+	author: String,
+	createdOn: {type: Date, "default": Date.now},
+	text: String;
+});
+
 var missionSchema = new mongoose.Schema({
 	name: {type: String, required: true},
 	rating: {type: Number, "default": 0, min: 0, max: 5},
 	author: String,
 	tag: [String],
-	coords: {type: [Number], index: '2dsphere'}
+	coords: {type: [Number], index: '2dsphere'},
+	time: timeSchema,
+	reviews: [reviewSchema]
 });
