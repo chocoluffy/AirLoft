38789c4a68c57ac4e6ab27943ead42da78ad4c56
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-14 10:16:01 -0400

    Designing db schema

--- app_server/models/missions.js
+++ app_server/models/missions.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var missionSchema = new mongoose.Schema({
-	name: String,
-	rating: {type: Number, "default": 0},
+	name: {type: String, required: true},
+	rating: {type: Number, "default": 0, min: 0, max: 5},
 	author: String,
-	tag: [String]
+	tag: [String],
+	coords: {type: [Number], index: '2dsphere'}
 });
