69adf7ec80c1fae59e47668f2cef3531342bf331
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-13 21:26:36 -0400

    Mongoose connection restart when nodemon restarts

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -9,8 +9,33 @@ mongoose.connection.on('connected', function(){
 
 mongoose.connection.on('error', function(err){
 	console.log('Mongoose connection error ' + err);
-})
+});
 
 mongoose.connection.on('disconnected', function(){
 	console.log('Mongoose disconnected');
+});
+
+var graceShutDown = function(msg, callback){
+	mongoose.connection.close(function(){
+		console.log('Mongoose is closed through ' + msg);
+		callback();
+	});
+};
+
+process.once('SIGUSR2', function(){
+	graceShutDown('nodemon restart', function(){
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+
+process.once('SIGINT', function(){
+	graceShutDown('app termination', function(){
+		process.exit(0);
+	});
+});
+
+process.once('SIGTERM', function(){
+	graceShutDown('Heroku app shutdown', function(){
+		process.exit(0);
+	})
 })
