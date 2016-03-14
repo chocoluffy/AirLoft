ea7231be031bcbdf78735044b999994b4f756206
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-14 16:40:42 -0400

    config testing production database

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 var dbURI = 'mongodb://localhost/airloft';
+
+if(process.env.NODE_ENV === 'production'){
+	dbURI = process.env.MONGOLAB_URI;
+}
+
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function(){
