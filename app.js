99f0d480f61d326673eb13096e0ecfa8120e47ff
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-20 21:49:29 -0400

    Adding all routing information for missions and reviews

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/api', routesApi);
 app.use('/home', routes);
 app.use('/users', users);
 
