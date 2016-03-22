d5de851cf381c0e7f4e0a962b2b1a57893d0e62e
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-21 20:32:14 -0400

    Update error handling for querying reviews

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -11,21 +11,37 @@ module.exports.reviewsCreate = function(req, res){
 }
 
 module.exports.reviewsReadOne = function(req, res){
-	Missions
-		.findById(req.params.missionid)
-		.select('name reviews')
-		.exec(function(err, mission){
-			var response, review;
-			review = mission.reviews.id(req.params.reviewid);
-			response = {
-				mission: {
-					name: mission.name,
-					id: req.params.missionid
-				},
-				reviews: review
-			};
-			sendJsonRes(res, 200, response);
-		});
+	if(req.params && req.params.missionid){
+		Missions
+			.findById(req.params.missionid)
+			.select('name reviews')
+			.exec(function(err, mission){
+				if(!mission){
+					sendJsonRes(res, 404, {
+						"message": "missionid not found"
+					})
+					return;
+				}else if(err){
+					sendJsonRes(res, 404, err);
+					return;
+				}
+				var response, review;
+				review = mission.reviews.id(req.params.reviewid);
+				response = {
+					mission: {
+						name: mission.name,
+						id: req.params.missionid
+					},
+					reviews: review
+				};
+				sendJsonRes(res, 200, response);
+			});
+	}else{
+		sendJsonRes(res, 404, {
+			"message": "No missionid in request"
+		})
+	}
+
 }
 
 module.exports.reviewsUpdateOne = function(req, res){
