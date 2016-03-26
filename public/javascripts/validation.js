746d0f31a66ad4f0b8354d62f58accc2afaec2de
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-26 11:44:56 -0400

    Update validation.js for form field checking

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -2,7 +2,7 @@ $("#addReview").submit(function(e){
 	$(".alert.alert-danger").hide();
 	if(!$("input#name").val() || !$("select#rating").val() || !$("textarea#review").val()){
 		if($(".alert.alert-danger").length){
-			$(".alert.alert-danger").show("slow");
+			$(".alert.alert-danger").show();
 			console.log("jquery!");
 		}else{
 			$(this).prepend('<div role="alert" class="alert alert-danger text-center">All fields required, please try again!</div>');
