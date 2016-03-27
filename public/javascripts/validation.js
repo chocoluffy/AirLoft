3a33ce83e13c57c16283eccd551d7cd81bcaea73
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-03-26 20:58:56 -0400

    Add one more seed and fix validation.js twinkling display

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -2,8 +2,7 @@ $("#addReview").submit(function(e){
 	$(".alert.alert-danger").hide();
 	if(!$("input#name").val() || !$("select#rating").val() || !$("textarea#review").val()){
 		if($(".alert.alert-danger").length){
-			$(".alert.alert-danger").show("slow");
-			console.log("jquery!");
+			$(".alert.alert-danger").show();
 		}else{
 			$(this).prepend('<div role="alert" class="alert alert-danger text-center">All fields required, please try again!</div>');
 			$(".alert.alert-danger").hide();
